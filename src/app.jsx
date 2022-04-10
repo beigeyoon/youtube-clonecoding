231f5994db97b74def2411b6dfd637e925b4d867
beigeyoon <beige.yoon@gmail.com>
2022-04-11 01:12:00 +0900

    show video list with title

---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list';
+
+function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA3TwJlVnzhwObl6_4iFwGkgx1idBq8F3w", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
+  return (
+    <VideoList videos={videos} />
+  );
+}
+
+export default App;
