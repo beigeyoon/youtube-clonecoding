7452f9021853b1a1377171c403a8795691a60dad
beigeyoon <beige.yoon@gmail.com>
2022-04-13 02:26:19 +0900

    Finished

--- src/components/video_detail.jsx
+++ src/components/video_detail.jsx
@@ -10,8 +10,8 @@ const VideoDetail = ({ video }) => {
         width="100%"
         height="500px"
         src={`https://www.youtube.com/embed/${video.id}`}
-        frameborder="0"
-        allowfullscreen
+        frameBorder="0"
+        allowFullScreen
       ></iframe>
       <h2>{video.snippet.title}</h2>
       <h3>{video.snippet.channelTitle}</h3>
